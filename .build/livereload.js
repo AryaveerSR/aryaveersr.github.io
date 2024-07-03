6f1bbb4946a9e8bb88e4be2ffa124db55fffe653
AryaveerSR <51504825+AryaveerSR@users.noreply.github.com>
2024-07-03 20:38:09 +0530

    site generator works

--- .build/livereload.js
+++
@@ -1,4 +0,0 @@
-const socketUrl = "ws://localhost:!!PORT!!"; // The script replaces this with the actual port
-
-let socket = new WebSocket(socketUrl);
-socket.addEventListener("message", (msg) => location.reload());
